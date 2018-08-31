a57291ecdfcef6a13502b08edc26605491d473f5
Sebastian Riedel <git@basxto.de>
2018-08-31 08:04:07 +0200

    Find mutliple words

--- script/search.js
+++ script/search.js
@@ -1,20 +1,24 @@
 $(document).ready(function(){
     $("input[name=searchbox]").keyup(function(e) {
         //console.log(e.target.value);
-        let query = e.target.value;
+        let query = e.target.value.split(' ');
         console.log(query);
         //each game has it's own row
         $("tr:has(label)").each(function(){
             let that = this;
+            //show by default
+            let display = '';
             //this should be just one one element
             $(this).find('label').each(function(){
-                //compare strings case insensitive
-                if($(this).text().toLowerCase().indexOf(query.toLowerCase()) != -1){
-                    $(that).css('display', '');
-                }else{
-                    $(that).css('display', 'none');
+                for(let i = 0;  i < query.length; ++i){
+                    //compare strings case insensitive
+                    if($(this).text().toLowerCase().indexOf(query[i].toLowerCase()) == -1){
+                        //hide if at least one mismatch
+                        display = 'none';
+                    }
                 }
             });
+            $(this).css('display', display);
         })
     });
 });
